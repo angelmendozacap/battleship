46152e65b696d72124542d286be7ef3603b7d4dc
Alfredo Mendoza <1930.kick@gmail.com>
2021-05-29 18:03:45 -0500

    🎉 Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // Or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
