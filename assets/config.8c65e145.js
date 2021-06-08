72d16ae8ee2b40065f796c051c2e223c7b0d6169
angelmendozacap <angelmendozacap@users.noreply.github.com>
2021-06-08 23:57:41 +0000

    deploy: 7b75ecc038a8f259dfe4c85e7de186ce95a8b8e6

--- assets/config.8c65e145.js
+++
@@ -1,1 +0,0 @@
-import{u as t,v as n,x as s}from"./vendor.4873629a.js";const e=n({numberOfAttempts:0});function i(){const n=s((()=>[{value:1/0,description:"Fácil (turnos infinitos)"},{value:100,description:"Medio (100 turnos)"},{value:50,description:"Difícil (50 turnos)"}]));return t({config:e,setAttempts:function(t){e.numberOfAttempts=Number(t)},defaultAttemptsOptions:n})}export{i as u};
